e72f1e92b6e616e95e78f056d4ec764ca0cd8ce4
Sean Killeen <SeanKilleen@gmail.com>
2019-06-29 14:54:28 -0400

    basic actor system

---
+++ src/UpForGrabber.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Akka.Actor;
+
+namespace UpForGrabber.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var system = ActorSystem.Create("upforgrabber");
+        }
+    }
+}
