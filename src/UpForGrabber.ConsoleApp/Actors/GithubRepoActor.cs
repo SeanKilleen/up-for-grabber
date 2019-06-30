c28321e456c0a363ffdd48227195ce423fe32fe7
Sean Killeen <SeanKilleen@gmail.com>
2019-06-29 21:03:49 -0400

    Log things

--- src/UpForGrabber.ConsoleApp/Actors/GithubRepoActor.cs
+++ src/UpForGrabber.ConsoleApp/Actors/GithubRepoActor.cs
@@ -1,16 +1,20 @@
 ﻿using Akka.Actor;
+using Akka.Event;
+using Akka.Logger.Serilog;
 
 namespace UpForGrabber.ConsoleApp.Actors
 {
     public class GithubRepoActor : ReceiveActor
     {
         private readonly BasicRepoInfo _repoInfo;
-
+        private readonly ILoggingAdapter _logger;
         public GithubRepoActor(BasicRepoInfo repoInfo)
         {
+            _logger = Context.GetLogger();
             _repoInfo = repoInfo;
 
             Become(Normal);
+            _logger.Info("Created actor at {Path}", Self.Path);
 
             // TODO: Tell self to start examining labels
         }
