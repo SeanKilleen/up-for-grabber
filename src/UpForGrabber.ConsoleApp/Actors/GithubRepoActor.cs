4b4ee1bc9c9b7db938137300147b50a1cd46d774
Sean Killeen <SeanKilleen@gmail.com>
2019-06-29 21:24:07 -0400

    log matching labels

--- src/UpForGrabber.ConsoleApp/Actors/GithubRepoActor.cs
+++ src/UpForGrabber.ConsoleApp/Actors/GithubRepoActor.cs
@@ -1,4 +1,7 @@
-﻿using Akka.Actor;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
 using Akka.Event;
 
 namespace UpForGrabber.ConsoleApp.Actors
@@ -8,6 +11,7 @@ namespace UpForGrabber.ConsoleApp.Actors
         private readonly BasicRepoInfo _repoInfo;
         private readonly ILoggingAdapter _logger;
         private readonly ActorSelection _githubClient;
+        private List<string> _upForGrabsLabels;
 
         public GithubRepoActor(BasicRepoInfo repoInfo)
         {
@@ -26,8 +30,51 @@ namespace UpForGrabber.ConsoleApp.Actors
         {
             Receive<Messages.Messages.LabelsForRepo>(msg =>
             {
-                
+                var matchingLabels = msg.Labels.Where(LabelIsPotentiallyUpForGrabs).ToList();
+
+                if (!matchingLabels.Any())
+                {
+                    _logger.Info("No matching up-for-grabs labels for {RepoName} -- stopping", _repoInfo.RepoFullName);
+                    return;
+                }
+
+                _logger.Info("Found {MatchingLabelCount} potential up-for-grabs labels for {RepoName}: {LabelsList}", matchingLabels.Count, _repoInfo.RepoFullName, matchingLabels);
+
+                _upForGrabsLabels = matchingLabels;
             });
         }
+
+        private static bool LabelIsPotentiallyUpForGrabs(string name)
+        {
+            return ThingsToSearch.Any(x => name.Contains(x, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        /// <remarks>Brainstormed from up-for-grabs.net</remarks>
+        private static readonly List<string> ThingsToSearch = new List<string>
+        {
+            // Used jQuery on up-for-grabs: $("p.label > a").each(function(index){console.log($(this).text())})
+            "up for",
+            "up-for",
+            "first",
+            "help",
+            "bitesize",
+            "teaser",
+            "easy",
+            "novice",
+            "trivial",
+            "junior",
+            "jump",
+            "first",
+            "newbie",
+            "quick",
+            "welcome",
+            "low-hanging",
+            "low hanging",
+            "low_hanging",
+            "you take it",
+            "new",
+            "accepting"
+        };
+
     }
 }
